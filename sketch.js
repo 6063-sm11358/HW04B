dd582ff7299df31457087e21fb9816850aa27741
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2023-09-29 13:23:04 -0400

    Code Updated

--- sketch.js
+++ sketch.js
@@ -1,5 +1,30 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function mouseMoved()
+{
+  fill(int(random(100,256)),0,0);
+  ellipse(mouseX,mouseY, int(random(100,200)), int(random(100,200)));
+  if(key=='s' && keyIsPressed)
+  {
+    rect(mouseX, mouseY, int(random(100,200)), int(random(100,200)));
+  }
+}
+
+function mouseDragged()
+{
+  fill(0,int(random(100,256)),0);
+  ellipse(mouseX,mouseY, int(random(100,200)), int(random(100,200)));
+}
+
+function keyPressed()
+{
+  if(key=='r')
+  {
+    background(255);
+  }
 }
 
 function draw() {
